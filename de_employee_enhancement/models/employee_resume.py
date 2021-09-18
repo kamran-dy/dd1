78e2908816b20b96ba7ec1f25e93eb5f76041554
dynexcel-ddc1 <ddc1@dynexce.co>
2021-09-18 19:07:21 +0500

    Update employee_resume.py

--- de_employee_enhancement/models/employee_resume.py
+++ de_employee_enhancement/models/employee_resume.py
@@ -7,12 +7,12 @@ from dateutil.relativedelta import relativedelta
 class HrResumeLineInh(models.Model):
     _inherit = 'hr.resume.line'
 
-    institute = fields.Char('Institute')
-    reason_to_leave = fields.Char('Reason To Leave')
-    salary = fields.Float('Salary')
-    cgpa = fields.Char('Division/CGPA')
-    is_experience = fields.Boolean('Is Experience?', default=False)
-    is_education = fields.Boolean('Is Education?', default=False)
+    institute = fields.Char(string='Institute')
+    reason_to_leave = fields.Char(string='Reason To Leave')
+    salary = fields.Float(string='Salary')
+    cgpa = fields.Char(string='Division/CGPA')
+    is_experience = fields.Boolean(string='Is Experience?', default=False)
+    is_education = fields.Boolean(string='Is Education?', default=False)
 
     @api.onchange('line_type_id')
     def compute_experience(self):
